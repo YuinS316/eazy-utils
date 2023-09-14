c264510e3094f8215cf276e6f7e0b68b400d2137
YuinS316 <qq724655292@163.com>
2023-09-14 20:26:33 +0800

    feat: add concurrent limit test unit cases

--- src/tools/concurrentLimit/__tests__/index.spec.ts
+++ src/tools/concurrentLimit/__tests__/index.spec.ts
@@ -10,7 +10,7 @@ describe('concurrent limit', () => {
     vi.useRealTimers();
   });
 
-  it('should work', () => {
+  it('should work', async () => {
     let id = 0;
 
     const createTask = (timeout: number) => {
@@ -26,11 +26,13 @@ describe('concurrent limit', () => {
     const p2 = cLimit(() => createTask(2000));
     const p3 = cLimit(() => createTask(3000));
 
-    const p4 = cLimit(() => createTask(4000));
-
-    vi.advanceTimersByTime(2000);
+    //  如果没有并发限制的话，前进到第3000毫秒，此时三个都已经resolve
+    //  但是存在了并发限制，p3会在第1000毫秒启动，于第4000毫秒resolve
+    await vi.advanceTimersByTimeAsync(3000);
     expect(p1).resolves.toBe(0);
     expect(p2).resolves.toBe(1);
-    // expect(p3).not.resolves.toBe(2);
+    expect(vi.getTimerCount()).toBe(1);
+    await vi.advanceTimersByTimeAsync(1000);
+    expect(p3).resolves.toBe(2);
   });
 });
